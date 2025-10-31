3d1d46252342c6ce4564679cd7c2491599f34fd2
João Gabriel <contato.joaogabriel@proton.me>
2025-10-31 14:04:18 -0300

    feat: blogpostinggit add *.js!

--- components/header/header.js
+++ components/header/header.js
@@ -33,53 +33,6 @@ class Header extends HTMLElement {
 
 		shadow.adoptedStyleSheets.push(css);
 	}
-	async getElements() {
-		const body = document.querySelector('body');
-		const small = this._shadowRoot.querySelectorAll('small');
-		small[0].style.cursor = 'pointer';
-
-		const title = [
-			document.querySelectorAll('h1'),
-			this._shadowRoot.querySelectorAll('h1'),
-			document.querySelectorAll('h2'),
-			document.querySelectorAll('h3'),
-			document.querySelectorAll('h4'),
-		];
-
-		const text = [
-			document.querySelectorAll('p'),
-			document.querySelectorAll('address'),
-			document.querySelectorAll('a'),
-			document.querySelectorAll('small'),
-			document.querySelectorAll('summary'),
-			document.querySelectorAll('li'),
-			small,
-		];
-
-		return [body, small, title, text];
-	}
-	async themeSwitcher() {
-		const [body, small, title, text] = await this.getElements();
-
-		function changeThemes() {
-			body.classList.toggle('darkMode');
-			text.forEach((_) => _.forEach((el) => el.classList.toggle('darkModeText')));
-			title.forEach((_) => _.forEach((el) => el.classList.toggle('darkModeTitle')));
-			small[0].textContent = '[turn on the lights]';
-
-			if (body.classList.contains('darkMode')) {
-				small[0].textContent = '[turn on the lights]';
-			} else {
-				small[0].textContent = '[turn off the lights]';
-			}
-		}
-
-		if (JSON.parse(localStorage.getItem('themePreference')) === 'dark') changeThemes();
-
-		small[0].addEventListener('click', function(event) {
-			changeThemes();
-		});
-	}
 }
 
 customElements.define('app-header', Header);
--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
 function savePreferences() {
 	const theme = document.querySelector('body').classList.contains('darkMode');
-	
-	if (theme)
-		localStorage.setItem('themePreference', JSON.stringify('dark'));
-	else
-		localStorage.setItem('themePreference', JSON.stringify('light'));
+
+	if (theme) localStorage.setItem('themePreference', JSON.stringify('dark'));
+	else localStorage.setItem('themePreference', JSON.stringify('light'));
 }
 
 async function loadProjects() {
 	const response = await fetch('../data/projects.json');
-	
-	if (!response.ok)
-		return false;
-	
-	const projects = await response.json();
-	const projectContainer = document.querySelector('.projectsContainer')
 
-	projects.forEach(function(project){
+	if (!response.ok) return false;
+
+	const projects = await response.json();
+	const projectContainer = document.querySelector('.projectsContainer');
+
+	projects.forEach(function(project) {
 		const projectHolder = document.createElement('section');
 
 		const projectTitle = document.createElement('h3');
@@ -27,20 +24,20 @@ async function loadProjects() {
 
 		projectHolder.appendChild(projectTitle);
 
-		if(project.link) {
+		if (project.link) {
 			const projectLink = document.createElement('a');
 			projectLink.href = project.link;
 			projectLink.target = '_blank';
 			projectLink.textContent = '[link]';
 			projectLink.style.fontSize = '0.8rem';
-			
+
 			projectHolder.appendChild(projectLink);
 		} else {
 			const projectLink = document.createElement('p');
 			projectLink.textContent = '[no link/referral]';
 			projectLink.style.fontSize = '0.8rem';
 			projectHolder.appendChild(projectLink);
-		} 
+		}
 
 		projectHolder.appendChild(projectDescription);
 
@@ -56,10 +53,81 @@ async function loadProjects() {
 		}
 
 		projectContainer.append(projectHolder);
-	})
+	});
 }
 
-(async function(){
+async function loadBlogposts() {
+	const response = await fetch('../data/blogposts.json');
+
+	if (!response.ok) throw new Error('Não foi possivel carregar os projetos.');
+
+	const blogposts = await response.json();
+
+	if (!(blogposts.length >= 1)) throw new Error('JSON Vazio, não haverá blogposts.');
+
+	blogposts.forEach((blogpost) => {
+		const blogWrapper = document.createElement('p');
+		blogWrapper.classList.add('blogAnchor');
+
+		blogWrapper.innerHTML = `> <strong class="blogAnchorTitle">${blogpost.title}</strong> - ${blogpost.author}.`;
+		blogWrapper.addEventListener('click', (event) => generatePostView(blogpost));
+		document.querySelector('.blogEntries').appendChild(blogWrapper);
+	});
+}
+
+function generatePostView(blogpost) {
+	const modal = document.createElement('dialog');
+	const tags = document.createElement('small');
+	const body = document.querySelector('body');
+
+	if (blogpost.tags.length >= 1) blogpost.tags.forEach((_tag) => (tags.textContent += `[${_tag}] `));
+	else tags.textContent = '[no-tags]';
+
+	const header = document.createElement('header');
+	const leaveButton = document.createElement('small');
+	const title = document.createElement('h2');
+
+	header.classList.add('blogpostHeader');
+	title.classList.add('sectionHeader');
+	title.textContent = blogpost.title;
+
+	leaveButton.textContent = '[leave post]';
+	leaveButton.style.cursor = 'pointer';
+	leaveButton.style.fontSize = '0.9rem';
+
+	header.append(title);
+	header.append(leaveButton);
+
+	const section = document.createElement('section');
+	section.classList.add('sectionModal');
+	if (blogpost.content.length >= 1) blogpost.content.forEach((line) => (section.innerHTML += line));
+
+	modal.classList.add('postModal');
+	modal.append(header);
+	modal.append(tags);
+	modal.append(section);
+
+	body.style.transition = '0.2s ease-in-out';
+	body.style.filter = 'blur(5px)';
+
+	body.append(modal);
+	modal.showModal();
+
+	document.addEventListener('keydown', (event) => {
+		if (event.key === 'Escape') {
+			modal.close();
+			modal.remove();
+			body.style.filter = 'none';
+		}
+	});
+
+	leaveButton.addEventListener('click', (event) => {
+		modal.close();
+		body.style.filter = 'none';
+	});
+}
+(async function() {
 	window.addEventListener('beforeunload', savePreferences);
-	await loadProjects()
-}())
+	await loadProjects();
+	await loadBlogposts();
+})();
