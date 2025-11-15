7ccf1532402de3bf0efc021c246dd9f9487ff34e
João Gabriel <contato.joaogabriel@proton.me>
2025-11-15 04:39:17 -0300

    wip: search func

--- js/main.js
+++ js/main.js
@@ -1,53 +1,44 @@
-function savePreferences() {
-	const theme = document.querySelector('body').classList.contains('darkMode');
-
-	if (theme) localStorage.setItem('themePreference', JSON.stringify('dark'));
-	else localStorage.setItem('themePreference', JSON.stringify('light'));
-}
-
 async function loadProjects() {
-	const response = await fetch('../data/projects.json');
+	const response = await fetch("../data/projects.json");
 
 	if (!response.ok) return false;
 
 	const projects = await response.json();
-	const projectContainer = document.querySelector('.projectsContainer');
+	const projectContainer = document.querySelector(".projectsContainer");
 
 	projects.forEach(function(project) {
-		const projectHolder = document.createElement('section');
+		const projectHolder = document.createElement("section");
 
-		const projectTitle = document.createElement('h3');
+		const projectTitle = document.createElement("h3");
 		projectTitle.textContent = project.title;
 
-		const projectDescription = document.createElement('p');
+		const projectDescription = document.createElement("p");
 		projectDescription.innerHTML = project.description;
 
 		projectHolder.appendChild(projectTitle);
 
 		if (project.link) {
-			const projectLink = document.createElement('a');
+			const projectLink = document.createElement("a");
 			projectLink.href = project.link;
-			projectLink.target = '_blank';
-			projectLink.textContent = '[link]';
-			projectLink.style.fontSize = '0.8rem';
+			projectLink.target = "_blank";
+			projectLink.textContent = "[link]";
+			projectLink.style.fontSize = "0.8rem";
 
 			projectHolder.appendChild(projectLink);
 		} else {
-			const projectLink = document.createElement('p');
-			projectLink.textContent = '[no link/referral]';
-			projectLink.style.fontSize = '0.8rem';
+			const projectLink = document.createElement("p");
+			projectLink.textContent = "[no link/referral]";
+			projectLink.style.fontSize = "0.8rem";
 			projectHolder.appendChild(projectLink);
 		}
 
 		projectHolder.appendChild(projectDescription);
 
 		if (project.image) {
-			// const details = document.createElement('details');
-			// const summary = document.createElement('summary');
-			const projectImage = document.createElement('img');
+			const projectImage = document.createElement("img");
 
 			projectImage.src = project.image;
-			projectImage.width = '300px';
+			projectImage.width = "300px";
 
 			projectHolder.appendChild(projectImage);
 		}
@@ -57,77 +48,91 @@ async function loadProjects() {
 }
 
 async function loadBlogposts() {
-	const response = await fetch('../data/blogposts.json');
+	const response = await fetch("../data/blogposts.json");
 
-	if (!response.ok) throw new Error('Não foi possivel carregar os projetos.');
+	if (!response.ok) throw new Error("Não foi possivel carregar os projetos.");
 
 	const blogposts = await response.json();
 
-	if (!(blogposts.length >= 1)) throw new Error('JSON Vazio, não haverá blogposts.');
-
+	if (!(blogposts.length >= 1))
+		throw new Error("JSON Vazio, não haverá blogposts.");
+	let count = 0;
 	blogposts.forEach((blogpost) => {
-		const blogWrapper = document.createElement('p');
-		blogWrapper.classList.add('blogAnchor');
+		const blogWrapper = document.createElement("p");
+		blogWrapper.classList.add("blogAnchor");
 
-		blogWrapper.innerHTML = `> <strong class="blogAnchorTitle">${blogpost.title}</strong> - ${blogpost.author}.`;
-		blogWrapper.addEventListener('click', (event) => generatePostView(blogpost));
-		document.querySelector('.blogEntries').appendChild(blogWrapper);
+		blogWrapper.innerHTML = `${++count}. ${blogpost.title}${blogpost.date ? " - " + blogpost.date : ""}`;
+		blogWrapper.addEventListener("click", (event) =>
+			generatePostView(blogpost),
+		);
+		document.querySelector(".blogEntries").appendChild(blogWrapper);
 	});
+
+	return blogposts;
 }
 
 function generatePostView(blogpost) {
-	const modal = document.createElement('dialog');
-	const tags = document.createElement('small');
-	const body = document.querySelector('body');
+	const modal = document.createElement("dialog");
+	const tags = document.createElement("small");
+	const body = document.querySelector("body");
 
-	if (blogpost.tags.length >= 1) blogpost.tags.forEach((_tag) => (tags.textContent += `[${_tag}] `));
-	else tags.textContent = '[no-tags]';
+	if (blogpost.tags.length >= 1)
+		blogpost.tags.forEach((_tag) => (tags.textContent += `[${_tag}] `));
+	else tags.textContent = "[no-tags]";
 
-	const header = document.createElement('header');
-	const leaveButton = document.createElement('small');
-	const title = document.createElement('h2');
+	const header = document.createElement("header");
+	const leaveButton = document.createElement("small");
+	const title = document.createElement("h2");
 
-	header.classList.add('blogpostHeader');
-	title.classList.add('sectionHeader');
+	header.classList.add("blogpostHeader");
+	title.classList.add("sectionHeader");
 	title.textContent = blogpost.title;
 
-	leaveButton.textContent = '[leave post]';
-	leaveButton.style.cursor = 'pointer';
-	leaveButton.style.fontSize = '0.9rem';
+	leaveButton.textContent = "[leave post]";
+	leaveButton.style.cursor = "pointer";
+	leaveButton.style.fontSize = "0.9rem";
 
 	header.append(title);
 	header.append(leaveButton);
 
-	const section = document.createElement('section');
-	section.classList.add('sectionModal');
-	if (blogpost.content.length >= 1) blogpost.content.forEach((line) => (section.innerHTML += line));
+	const section = document.createElement("section");
+	section.classList.add("sectionModal");
+	if (blogpost.content.length >= 1)
+		blogpost.content.forEach((line) => (section.innerHTML += line));
 
-	modal.classList.add('postModal');
+	modal.classList.add("postModal");
 	modal.append(header);
 	modal.append(tags);
 	modal.append(section);
 
-	body.style.transition = '0.2s ease-in-out';
-	body.style.filter = 'blur(5px)';
+	body.style.transition = "0.2s ease-in-out";
+	body.style.filter = "blur(5px)";
 
 	body.append(modal);
 	modal.showModal();
 
-	document.addEventListener('keydown', (event) => {
-		if (event.key === 'Escape') {
+	document.addEventListener("keydown", (event) => {
+		if (event.key === "Escape") {
 			modal.close();
 			modal.remove();
-			body.style.filter = 'none';
+			body.style.filter = "none";
 		}
 	});
 
-	leaveButton.addEventListener('click', (event) => {
+	leaveButton.addEventListener("click", (event) => {
 		modal.close();
-		body.style.filter = 'none';
+		body.style.filter = "none";
 	});
 }
+
+function blogSearchHandler(element) {
+	// wip
+}
+
 (async function() {
-	window.addEventListener('beforeunload', savePreferences);
 	await loadProjects();
 	await loadBlogposts();
+	const input = document.getElementsByName("search-input")[0];
+
+	input.addEventListener("input", (event) => blogSearchHandler(input));
 })();
